1611dfa8d9941c90c60f4162467e858106a3495c
mateus.borja <mateus.borja@gmail.com>
2020-10-11 16:27:40 -0400

    add new classes

--- src/main/java/com/mateusborja/javabegin/aula1/Pessoa.java
+++ src/main/java/com/mateusborja/javabegin/aula1/Pessoa.java
@@ -15,27 +15,10 @@ package com.mateusborja.javabegin.aula1;
         this.nome = nome;
     }
 
-    public String getNome() {
-        return nome;
-    }
     
-    public int getCodigo() {
-        return codigo;
-    }
+    
+    
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    
-    public void setCodigo(int codigo) {
-        this.codigo = codigo;
-    }
-
-    
-    
-    
-    
-   
     
     
 }
